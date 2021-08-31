0e09d501382d27c2d01cb3ecb18aab0c844aed62
Nycole Becker Rodrigues <82180015+NycoleBecker@users.noreply.github.com>
2021-08-31 20:47:17 -0300

    Atualizando

--- src/Aula04/Produto.java
+++ src/Aula04/Produto.java
@@ -1,10 +1,13 @@
 package Aula04;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Produto {
     public static void main(String[] args) {
         String nomeProduto;
+        DecimalFormat df = new DecimalFormat();
+        df.applyPattern("#.00");
         final double PERCENTUAL=1.12;
         double valorProduto;
         Scanner tc = new Scanner(System.in);
@@ -18,7 +21,7 @@ public class Produto {
 
                 valorProduto=valorProduto*PERCENTUAL;
                 System.out.println("Produto: "+nomeProduto);
-                System.out.println("Novo preço do produto: R$"+valorProduto);
+                System.out.println("Novo preço do produto: R$"+df.format(valorProduto));
             }else{
                 System.out.println("PROGRAMA ENCERRADO. ATÉ MAIS!");
             }
--- src/Aula04/SenhaMestre.java
+++ src/Aula04/SenhaMestre.java
@@ -1,4 +1,15 @@
 package Aula04;
 
 public class SenhaMestre {
+    public static void main(String[] args) {
+        final int MAIOR=5;
+        int sorteio;
+        String senha ="";
+        sorteio=(int)(Math.random()*MAIOR+5);
+        System.out.println(sorteio);
+        for (int i=0; i<sorteio; i++){
+            senha += Integer.toString((int)(Math.random()*10));
+        }
+        System.out.println(senha);
+    }
 }
